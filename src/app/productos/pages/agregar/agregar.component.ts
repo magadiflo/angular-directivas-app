d0c184c9747fc5d49e96ef805aad06e62be41876
Martín <magadiflo@gmail.com>
2022-10-05 22:44:47 -0500

    Input setters

--- src/app/productos/pages/agregar/agregar.component.ts
+++ src/app/productos/pages/agregar/agregar.component.ts
@@ -9,6 +9,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class AgregarComponent {
 
+  color: string = 'red';
+  texto: string = 'Texto por defecto desde el componente Agregar';
+
   miFormulario: FormGroup = this.fb.group({
     nombre: ['', [Validators.required]],
   });
@@ -17,6 +20,14 @@ export class AgregarComponent {
 
   tieneError(campo: string): boolean {
     return this.miFormulario.get(campo)?.invalid || false;
+  }
+
+  cambiarColor(): void {
+    this.color = "#xxxxxx".replace(/x/g, y=>(Math.random()*16|0).toString(16));    
+  }
+
+  cambiarNombre(): void {
+    this.texto = Math.random().toString();
   }
 
 }
--- src/app/shared/directives/error-msg.directive.ts
+++ src/app/shared/directives/error-msg.directive.ts
@@ -5,26 +5,29 @@ import { Directive, OnInit, ElementRef, Input } from '@angular/core';
 })
 export class ErrorMsgDirective implements OnInit {
 
-  @Input() mensaje: string = 'Este campo es necesario';
-  @Input() color: string = 'red';
+  private _texto: string = 'Mensaje dentro de la directiva por defecto';
+  private _color: string = 'blue';
+
   htmlElement: ElementRef<HTMLElement>;
+
+  @Input() set mensaje(valor: string) {
+    this._texto = valor;
+    this.htmlElement.nativeElement.innerText = valor;
+  }
+
+  @Input() set color(valor: string) { //* Recibimos el color con el input (de afuera) y se le pasa al parámetro valor, de esta manera evitamos usar el ngOnChanges(...)
+    this._color = valor;
+    this.htmlElement.nativeElement.style.color = valor;
+  }
 
   constructor(private el: ElementRef<HTMLElement>) {
     this.htmlElement = el;
   }
 
   ngOnInit(): void {
-    this.setColor();
-    this.setMensaje();
     this.setClass();
-  }
-
-  setColor(): void {
-    this.htmlElement.nativeElement.style.color = this.color;
-  }
-
-  setMensaje(): void {
-    this.htmlElement.nativeElement.innerText = this.mensaje;
+    this.htmlElement.nativeElement.innerHTML = this._texto;
+    this.htmlElement.nativeElement.style.color = this._color;
   }
 
   setClass(): void {
